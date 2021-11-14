dd76dd214de508215d7580b34fc502188e1c0465
benzjakk <36098631+benzjakk@users.noreply.github.com>
2021-11-14 21:06:15 +0700

    fix upload to predict

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -8,23 +8,24 @@ export const Home = () => {
   const [previewImg, setPreviewImg] = useState(null)
   
   
-  function getBase64(file) {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => resolve(reader.result);
-      reader.onerror = error => reject(error);
-    });
-  }
+  // function getBase64(file) {
+  //   return new Promise((resolve, reject) => {
+  //     const reader = new FileReader();
+  //     reader.readAsDataURL(file);
+  //     reader.onload = () => resolve(reader.result);
+  //     reader.onerror = error => reject(error);
+  //   });
+  // }
 
   const handleAction = async(file) => {
-    console.log(file);
+    const data = new FormData();
+    data.append("image",file);
+    data.append("filename",file.name);
     const previewImgUrl = URL.createObjectURL(file);
     setPreviewImg(previewImgUrl);
     try {
-      const fileBase64 = await getBase64(file);
-      const res = await predict(fileBase64);
-      console.log('res',res);
+      const res = await predict(data);
+      console.log(res);
     } catch (error) {
       console.error(error);
     }
--- src/service/predict.js
+++ src/service/predict.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API_URL = 'https://whattheshark-backend.herokuapp.com/predict';
-export const predict = async(image) => {
-  const res = axios.post(API_URL, { image });
+export const predict = async(data) => {
+  const res = await axios.post(API_URL, data);
 
   return res.data;
 }
